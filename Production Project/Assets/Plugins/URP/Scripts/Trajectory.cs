d1c4760f327603dcc2ed5ec51902184a76c9b3c7
Amitvaknin1 <74488893+Amitvaknin1@users.noreply.github.com>
2022-04-06 22:01:54 +0300

    Added script + trajectory and line effcet

--- Production Project/Assets/Plugins/URP/Scripts/Trajectory.cs
+++ Production Project/Assets/Plugins/URP/Scripts/Trajectory.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Trajectory : MonoBehaviour
 {
-    [SerializeField]List<Vector3> pathVerysList = new List<Vector3>();
+    [SerializeField] List<Vector3> pathVerysList = new List<Vector3>();
     [SerializeField] float time = 1;
     [SerializeField] Vector3 velocity = Vector3.right;
     [SerializeField] Vector3 acceleration = Vector3.down;
+    [SerializeField] Vector3 unityAccuracyFix = Vector3.zero;
     [SerializeField] int splits = 3;
     // UT+.5 ATT
-    public float Distance_InTime_DueToAcc(float a, float t)
+    public float Distance_InTime_DueToAcc(float u, float a, float t)
     {
-        return 0.5f * a * t * t;
+        return u* 0.5f * a * t * t;
     }
     public void Calculate_Trajectory()
     {
@@ -21,23 +22,29 @@ public class Trajectory : MonoBehaviour
             pathVerysList = new List<Vector3>();
         }
         pathVerysList.Clear();
+         Vector3 d;
         float dt = 0;
         for (int i = 0; i < splits; i++)
         {
             dt = (time / (splits - 1)) * i;
-            Vector3 d = Vector3.up * Distance_InTime_DueToAcc(acceleration.y, dt);
-            d.x = velocity.x * dt;
+            d.x = Distance_InTime_DueToAcc(velocity.x, acceleration.x, dt);
+            d.y = Distance_InTime_DueToAcc(velocity.y, acceleration.y, dt);
+            d.z = Distance_InTime_DueToAcc(velocity.z, acceleration.z, dt);
             pathVerysList.Add(d);
         }
     }
 
     [Header("Ref")]
     [SerializeField] LineRenderer LineRendere = null;
+    [SerializeField] Rigidbody projectile;
 
 
     [Header("Ediotr Setting")]
     [SerializeField] bool calc_Trajectory = false;
     [SerializeField] bool auto_calc = false;
+
+    [Space]
+    [SerializeField] bool fire = false;
     private void OnDrawGizmosSelected()
     {
         if (calc_Trajectory || auto_calc)
@@ -46,6 +53,13 @@ public class Trajectory : MonoBehaviour
             Calculate_Trajectory();
             LineRendere.positionCount = splits;
             LineRendere.SetPositions(pathVerysList.ToArray());
+        }
+        if (fire)
+        {
+            fire = false;
+
+            projectile.transform.position = transform.position;
+            projectile.velocity = velocity + unityAccuracyFix;
         }
     }
 }
