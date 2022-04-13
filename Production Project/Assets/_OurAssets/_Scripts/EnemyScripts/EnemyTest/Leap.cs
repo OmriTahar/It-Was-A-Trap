4b8b20e4c9db030e518d1d188fc6291fe4644281
Omri Tahar <omritaharnew@gmail.com>
2022-04-13 18:10:00 +0300

    Attaching HP bar to incoming damage + fixing leaper damage

--- Production Project/Assets/_OurAssets/_Scripts/EnemyScripts/EnemyTest/Leap.cs
+++ Production Project/Assets/_OurAssets/_Scripts/EnemyScripts/EnemyTest/Leap.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class Leap : Attack
 {
 
+    bool _hasAttacked = false;
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !_hasAttacked)
+        {
+            other.gameObject.GetComponent<Unit>().RecieveDamage(this);
+            print("Leaped player!");
+            _hasAttacked = true;
+        }
+        else
+        {
+            print("Else!");
+        }
+    }
 }
--- Production Project/Assets/_OurAssets/_Scripts/EnemyScripts/EnemyTest/Unit.cs
+++ Production Project/Assets/_OurAssets/_Scripts/EnemyScripts/EnemyTest/Unit.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
+
     [SerializeField] internal int _unitHP;
+    [SerializeField] internal int _unitMaxHP;
     [SerializeField] internal int _unitRange;
+
+    [SerializeField] Slider _myHealthSlider;
+
+    private void Start()
+    {
+        _unitHP = _unitMaxHP;
+
+        if (_myHealthSlider != null)
+            _myHealthSlider.value = CalculateHealth();
+
+    }
 
     public void RecieveDamage(IAttackable<Unit> enemy)
     {
+        print("Slider Value Before: " + _myHealthSlider.value + " Player Health: " + _unitHP);
+
         enemy.Attack(this);
+
+        if (_myHealthSlider != null)
+            _myHealthSlider.value = CalculateHealth();
+
+        print("Slider Value After: " + _myHealthSlider.value + " Player Health: " + _unitHP);
+    }
+
+
+    float CalculateHealth()
+    {
+        return _unitHP / _unitMaxHP;
     }
 }
