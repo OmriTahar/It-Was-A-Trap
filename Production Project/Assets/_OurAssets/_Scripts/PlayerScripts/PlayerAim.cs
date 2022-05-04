a9da173e38d775cadd4faa1a49f56ae01e8a9268
Omri Tahar <omritaharnew@gmail.com>
2022-05-04 14:15:33 +0300

    Added first interactable (Wall - green rope to the right)

--- Production Project/Assets/_OurAssets/_Scripts/PlayerScripts/PlayerAim.cs
+++ Production Project/Assets/_OurAssets/_Scripts/PlayerScripts/PlayerAim.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerAim : MonoBehaviour
 {
+
     public static PlayerAim Instance;
+
+    [Header("Aim Refrences")]
     [SerializeField] Camera _cam;
     [SerializeField] GameObject _outlinePrefab, _line, _trapOutline, _WallOutline;
     [SerializeField] LayerMask _groundMask;
+
+    [Header("Aim Settings")]
     [SerializeField] float _maxDistance = 5f;
     [SerializeField] bool _active = false;
+
+    [Header("Interaction")]
+    [SerializeField] LayerMask _interactableLayers;
+    [SerializeField] bool _isAllowedToInteract;
+    [SerializeField] bool _canInteract;
+    [SerializeField] float _interactionDistance = 22f;
+    [SerializeField] GameObject _canInteractText;
+    private Interactable _currentInteractable;
+
     internal bool _canShoot = true;
     internal GameObject _outline;
     GameObject _currentAttackOutline;
@@ -29,12 +45,23 @@ public class PlayerAim : MonoBehaviour
         //_outline = Instantiate(_currentAttackOutline, transform);
         //remove this after we decide when we want aim to start V
         ToggleDraw();
+
+        _canInteractText.SetActive(false);
     }
 
     private void Update()
     {
+        print("Interaction distance: " + _interactionDistance);
+
         if (_active)
+        {
             UpdateAim();
+
+            if (Input.GetKeyDown(KeyCode.F) && _canInteract)
+            {
+                Interact();
+            }
+        }
     }
 
     private void UpdateAim()
@@ -55,12 +82,15 @@ public class PlayerAim : MonoBehaviour
                 _outline.transform.position = transform.position + distanceVector.normalized * _maxDistance;
                 _outline.transform.position = new Vector3(_outline.transform.position.x, hit.point.y, _outline.transform.position.z);
             }
+
             //Debug.DrawLine(ray.origin, hit.point, Color.red);
         }
         else
         {
             _canShoot = false;
         }
+
+        InteractionCheck(hit, ray);
     }
 
     public void ToggleDraw()
@@ -70,4 +100,30 @@ public class PlayerAim : MonoBehaviour
         //_line.SetActive(active);
     }
 
+    private void InteractionCheck(RaycastHit hit, Ray ray)
+    {
+        if (_isAllowedToInteract)
+        {
+            if (Physics.Raycast(ray, out hit, _interactionDistance, _interactableLayers))
+            {
+                _canInteract = true;
+                _currentInteractable = hit.collider.GetComponent<Interactable>();
+                _canInteractText.SetActive(true);
+            }
+            else
+            {
+                _canInteract = false;
+                _currentInteractable = null;
+                _canInteractText.SetActive(false);
+            }
+        }
+    }
+
+    public void Interact()
+    {
+        if (_currentInteractable)
+        {
+            _currentInteractable.OnInteraction();
+        }
+    }
 }
