e303bfea4426b8e490668da1ba0690ed87eeaf31
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-06-26 19:55:39 +0300

    Menus progress

--- Production Project/Assets/_OurAssets/_Scripts/UI/MenuManager.cs
+++ Production Project/Assets/_OurAssets/_Scripts/UI/MenuManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 
 public enum MenuFunc { Main, Settings, Credits, Save, Load}
 public class MenuManager : MonoBehaviour
 {
     public static MenuManager instance;
-    [SerializeField] GameObject[] iconArray;
+    [SerializeField] GameObject[] settingCategories;
+    [SerializeField] GameObject[] settingUnderlines;
+    [SerializeField] Image[] iconArray;
     [SerializeField] Menu[] menus;
+
+    Color full = new Color(1, 1, 1, 1);
+    Color transparent = new Color(1, 1, 1, 0);
 
     private void Awake()
     {
@@ -48,16 +54,37 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void SelectSettingCategory(int settingNum)
+    {
+        if (!settingCategories[settingNum].activeSelf)
+        {
+            foreach (GameObject category in settingCategories)
+                if (category)
+                    category.SetActive(false);
+
+            foreach (GameObject underline in settingUnderlines)
+                if (underline)
+                    underline.SetActive(false);
+
+            if (settingCategories[settingNum])
+                settingCategories[settingNum].SetActive(true);
+
+            if (settingUnderlines[settingNum])
+                settingUnderlines[settingNum].SetActive(true);
+
+        }
+    }
+
     public void OnHoverEnter(int buttonNum)
     {
         if (iconArray[buttonNum])
-            iconArray[buttonNum].gameObject.SetActive(true);
+            iconArray[buttonNum].color = full;
     }
 
     public void OnHoverExit(int buttonNum)
     {
         if (iconArray[buttonNum])
-            iconArray[buttonNum].gameObject.SetActive(false);
+            iconArray[buttonNum].color = transparent;
     }
 
 }
