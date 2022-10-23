a9bd517335f2c3eacc0226d237309cb599ea6acf
Amitvaknin1 <74488893+Amitvaknin1@users.noreply.github.com>
2022-10-23 20:58:16 +0300

    opening  bug fix

--- Production Project/Assets/_OurAssets/_Scripts/UI/MenuManager.cs
+++ Production Project/Assets/_OurAssets/_Scripts/UI/MenuManager.cs
@@ -51,7 +51,7 @@ public class MenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/20_s music game mix Am");
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Music");
         SceneManager.LoadScene(1);
     }
 
